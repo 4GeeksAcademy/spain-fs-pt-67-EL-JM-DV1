f433cb1245d4608b256e3607d97de6b4dfeec8f1
Eduardo Loreto <138321528+EduardoLoreto@users.noreply.github.com>
2024-07-22 18:01:49 +0000

    Endpoints

--- src/app.py
+++ src/app.py
@@ -2,13 +2,14 @@
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
 import os
-from flask import Flask, request, jsonify, url_for
+from flask import Flask, request, jsonify, url_for, send_from_directory
 from flask_migrate import Migrate
 from flask_swagger import swagger
-from flask_cors import CORS
-from utils import APIException, generate_sitemap
-from admin import setup_admin
-from models import db, User, Product, OrderItems, Order
+from api.utils import APIException, generate_sitemap
+from api.routes import api
+from api.admin import setup_admin
+from api.commands import setup_commands
+from api.models import db, User, Product, OrderItems, Order
 
 
 app = Flask(__name__)
