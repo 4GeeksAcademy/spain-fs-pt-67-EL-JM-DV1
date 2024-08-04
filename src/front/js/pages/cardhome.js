0002ff146a8ccd4be9418e5701798c3a9e1f87cd
Eduardo Loreto <138321528+EduardoLoreto@users.noreply.github.com>
2024-08-04 14:34:58 +0000

    Creacion de Cardhome y home

---
+++ src/front/js/pages/cardhome.js
@@ -0,0 +1,35 @@
+import React from "react";
+
+const Card = ({ cards }) => {
+    return (
+        <div className="container-fluid-center justify-content-center mt-5 mb-5 row col-lg-6 col-md-6 col-sm-12">
+            <div className="container-fluid justify-content-center row col-md-6 col-sm-6 gap-3">
+                {cards.slice(0, 3).map((card, index) => (
+                    <div key={index} className="card" style={{ width: "18rem" }}>
+                        <img src={card.image} className="card-img-top" alt={card.title} />
+                        <div className="card-body">
+                            <h5 className="card-title">{card.title}</h5>
+                            <p className="card-text">{card.text}</p>
+                            <a href={card.link} className="btn btn-danger text-center w-50">comprar</a>
+                        </div>
+                    </div>
+                ))}
+            </div>
+
+            <div className="container-fluid justify-content-center row col-md-6 col-sm-6 gap-3">
+                {cards.slice(3, 6).map((card, index) => (
+                    <div key={index} className="card" style={{ width: "18rem" }}>
+                        <img src={card.image} className="card-img-top" alt={card.title} />
+                        <div className="card-body">
+                            <h5 className="card-title">{card.title}</h5>
+                            <p className="card-text">{card.text}</p>
+                            <a href={card.link} className="btn btn-danger text-center w-50">comprar</a>
+                        </div>
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
+};
+
+export default Card;
