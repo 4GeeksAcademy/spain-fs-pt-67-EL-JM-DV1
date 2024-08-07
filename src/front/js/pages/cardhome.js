7ca4021d256e198177b7461dabf0474bb1287843
Eduardo Loreto <138321528+EduardoLoreto@users.noreply.github.com>
2024-08-07 18:11:12 +0000

    Culminacion de Home

--- src/front/js/pages/cardhome.js
+++ src/front/js/pages/cardhome.js
@@ -3,27 +3,27 @@ import React from "react";
 const Card = ({ cards }) => {
     return (
         <div className="container-fluid-center justify-content-center mt-5 mb-5 row col-lg-6 col-md-6 col-sm-12">
-            <div className="container-fluid justify-content-center row col-md-6 col-sm-6 gap-3">
+            <div className="container-fluid-center justify-content-center row col-md-6 col-sm-6 gap-3">
                 {cards.slice(0, 3).map((card, index) => (
-                    <div key={index} className="card" style={{ width: "18rem" }}>
+                    <div key={index} className="card text-center" style={{ width: "18rem" }}>
                         <img src={card.image} className="card-img-top" alt={card.title} />
                         <div className="card-body">
                             <h5 className="card-title">{card.title}</h5>
                             <p className="card-text">{card.text}</p>
-                            <a href={card.link} className="btn btn-danger text-center w-50">comprar</a>
+                            <a href={card.link} className="btn btn-danger text-center w-50">Comprar</a>
                         </div>
                     </div>
                 ))}
             </div>
 
-            <div className="container-fluid justify-content-center row col-md-6 col-sm-6 gap-3">
+            <div className="container-fluid-center justify-content-center row col-md-6 col-sm-6 gap-3">
                 {cards.slice(3, 6).map((card, index) => (
-                    <div key={index} className="card" style={{ width: "18rem" }}>
+                    <div key={index} className="card text-center" style={{ width: "18rem" }}>
                         <img src={card.image} className="card-img-top" alt={card.title} />
                         <div className="card-body">
                             <h5 className="card-title">{card.title}</h5>
                             <p className="card-text">{card.text}</p>
-                            <a href={card.link} className="btn btn-danger text-center w-50">comprar</a>
+                            <a href={card.link} className="btn btn-danger text-center w-50">Comprar</a>
                         </div>
                     </div>
                 ))}
