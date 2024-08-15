87c4005222f722aaae2a77ce5a668d166ec1d75b
DVBDJ <163288149+DVBDJ@users.noreply.github.com>
2024-08-15 18:24:42 +0000

    cambios

--- src/front/js/pages/registro.js
+++ src/front/js/pages/registro.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { getState } from '../flux/store';
+
 
 export function Registro() {
     const [name, setName] = useState("");
